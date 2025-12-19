3beb5793d1cd05fb28b19c27125639a2eb9b9d7b
Josh Johnston <josh@x-team.com>
2025-12-19 11:55:09 +1100

    Add form fields with placeholder values

--- index.js
+++ index.js
@@ -19,6 +19,16 @@ const { PRIVATE_APP_ACCESS } = process.env;
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
 // * Code for Route 2 goes here
+app.get('/update-cobj', (req, res) => {
+  const contactEmail = ''
+  const quest = {}
+
+  res.render('updates', {
+    title: 'Update Custom Object Form | Integrating With HubSpot I Practicum',
+    contactEmail,
+    quest
+  })
+})
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
