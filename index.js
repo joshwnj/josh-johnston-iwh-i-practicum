bc7a33e9b6bb951bb40009ff96b75f1cf8788881
Josh Johnston <josh@x-team.com>
2025-12-19 12:29:11 +1100

    Add homepage with table of objects

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ app.use(express.json());
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
 const { PRIVATE_APP_ACCESS } = process.env;
 
+const SITE_TITLE = 'Integrating With HubSpot I Practicum'
+
 // TODO: fetch from schema
 const OBJECT_TYPE_IDS = {
   QUESTS: '2-54772188'
@@ -20,6 +22,22 @@ const OBJECT_TYPE_IDS = {
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
 // * Code for Route 1 goes here
+app.get('/', async (req, res) => {
+  const url = `https://api.hubapi.com/crm/v3/objects/${OBJECT_TYPE_IDS.QUESTS}?properties=name,description,reward`
+  const headers = {
+      Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+      'Content-Type': 'application/json'
+  };
+
+  try {
+      const resp = await axios.get(url, { headers } );
+      const data = resp.data.results;
+
+      res.render('homepage', { title: `Home | ${SITE_TITLE}`, data });
+  } catch(err) {
+      console.error(err);
+  }
+})
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
@@ -29,7 +47,7 @@ app.get('/update-cobj', (req, res) => {
   const quest = {}
 
   res.render('updates', {
-    title: 'Update Custom Object Form | Integrating With HubSpot I Practicum',
+    title: `Update Custom Object Form | ${SITE_TITLE}`,
     contactEmail,
     quest
   })
