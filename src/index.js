13253b2b86dc47e125f887e412bde5c86f6a0e22
Josh Johnston <josh@x-team.com>
2025-12-19 14:03:52 +1100

    move implementation code into src directory

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
+const path = require('path')
 const express = require('express');
 const app = express();
 
 const service = require('./service')
 
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
